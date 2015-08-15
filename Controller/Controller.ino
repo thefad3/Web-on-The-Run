1bbd026207c3f64f83dabbcf44f61539608b0500
vcabieles <vcabieles@fullsail.edu>
2015-08-15 13:38:25 -0400

    Car Controller Code

---
+++ Controller/Controller.ino
@@ -0,0 +1,115 @@
+
+/*
+* Getting Started example sketch for nRF24L01+ radios
+* This is an example of how to send data from one node to another using data structures
+* Updated: Dec 2014 by TMRh20
+*/
+#include <SPI.h>
+#include "RF24.h"
+
+byte addresses[][6] = {"1Node","2Node"};
+
+
+
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 0;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(9,10);
+/**********************************************************/
+
+
+/**
+* Create a data structure for transmitting and receiving data
+* This allows many variables to be easily sent and received in a single transmission
+* See http://www.cplusplus.com/doc/tutorial/structures/
+*/
+struct dataStruct{
+  unsigned long _micros;
+  float value;
+  float health;
+}myData;
+
+void setup() {
+
+  radio.begin();
+
+  Serial.begin(115200);
+
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+ // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
+  
+//  myData.value = 1.22;
+//  myData.health = 1337;
+  // Start the radio listening for data
+  radio.startListening();
+} // Setup Closing Tag
+
+
+
+
+void loop() {
+  
+ 
+/****************** Ping Out Role ***************************/  
+    radio.stopListening();                                    // First, stop listening so we can talk.
+
+      myData.value = 1.22;
+      myData.health = 9000;
+    
+    Serial.println(F("Now sending"));
+
+    myData._micros = micros();
+     if (!radio.write( &myData, sizeof(myData) )){
+       Serial.println(F("failed"));
+     }
+        
+    radio.startListening();                                    // Now, continue listening
+    
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+        
+    if ( timeout ){                                             // Describe the results
+        Serial.println(F("Failed, response timed out."));
+    }else{
+                                                                // Grab the response, compare, and send to debugging spew
+        radio.read( &myData, sizeof(myData) );
+        unsigned long time = micros();
+        
+        // Spew it
+        Serial.print(F("Sent "));
+        Serial.print(time);
+        Serial.print(F(", Got response "));
+        Serial.print(myData._micros);
+        Serial.print(F(", Round-trip delay "));
+        Serial.print(time-myData._micros);
+        Serial.print(F(" microseconds Value "));
+        Serial.println(myData.value);
+    }
+
+    // Try again 1s later
+    delay(1000);
+
+
+
+} // Loop
