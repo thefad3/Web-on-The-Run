602fc8c29156071719aa9405f04985e6f6d4b6c7
Chris Lynch <thefad3@fullsail.edu>
2015-08-15 13:35:36 -0400

    Commiting the right code

--- carBoard/carBoard.ino/carBoard.ino.ino
+++ carBoard/carBoard.ino/carBoard.ino.ino
@@ -1,27 +1,41 @@
 
 /*
 * Getting Started example sketch for nRF24L01+ radios
-* This is a very basic example of how to send data from one node to another
+* This is an example of how to send data from one node to another using data structures
 * Updated: Dec 2014 by TMRh20
 */
 
 #include <SPI.h>
 #include "RF24.h"
 
+byte addresses[][6] = {"1Node","2Node"};
+
+
 /****************** User Config ***************************/
 /***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 0;
+bool radioNumber =1;
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
 RF24 radio(9,10);
 /**********************************************************/
 
-byte addresses[][6] = {"1Node","2Node"};
 
 // Used to control whether this node is sending or receiving
 bool role = 0;
 
+/**
+* Create a data structure for transmitting and receiving data
+* This allows many variables to be easily sent and received in a single transmission
+* See http://www.cplusplus.com/doc/tutorial/structures/
+*/
+struct dataStruct{
+  unsigned long _micros;
+  float value;
+  float health;
+}myData;
+
 void setup() {
+
   Serial.begin(115200);
   Serial.println(F("RF24/examples/GettingStarted"));
   Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
@@ -30,7 +44,7 @@ void setup() {
 
   // Set the PA Level low to prevent power supply related issues since this is a
  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setPALevel(RF24_PA_MAX);
+  radio.setPALevel(RF24_PA_LOW);
   
   // Open a writing and reading pipe on each radio, with opposite addresses
   if(radioNumber){
@@ -41,9 +55,13 @@ void setup() {
     radio.openReadingPipe(1,addresses[1]);
   }
   
+  myData.value = 1.22;
   // Start the radio listening for data
   radio.startListening();
 }
+
+
+
 
 void loop() {
   
@@ -56,13 +74,8 @@ if (role == 1)  {
     
     Serial.println(F("Now sending"));
 
-    //unsigned long time = micros();                             // Take the time, and send it.  This will block until complete
-    unsigned long time = 1337;   
-    
-    // Take the time, and send it.  This will block until complete
-
-   
-     if (!radio.write( &time, sizeof(unsigned long) )){
+    myData._micros = micros();
+     if (!radio.write( &myData, sizeof(myData) )){
        Serial.println(F("failed"));
      }
         
@@ -81,18 +94,19 @@ if (role == 1)  {
     if ( timeout ){                                             // Describe the results
         Serial.println(F("Failed, response timed out."));
     }else{
-        unsigned long data;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &data, sizeof(unsigned long) );
+                                                                // Grab the response, compare, and send to debugging spew
+        radio.read( &myData, sizeof(myData) );
         unsigned long time = micros();
         
         // Spew it
         Serial.print(F("Sent "));
         Serial.print(time);
         Serial.print(F(", Got response "));
-        Serial.print(data);
+        Serial.print(myData._micros);
         Serial.print(F(", Round-trip delay "));
-        Serial.print(data);
-        Serial.println(F(" microseconds"));
+        Serial.print(time-myData._micros);
+        Serial.print(F(" microseconds Value "));
+        Serial.println(myData.value);
     }
 
     // Try again 1s later
@@ -105,19 +119,23 @@ if (role == 1)  {
 
   if ( role == 0 )
   {
-    unsigned long got_time;
     
     if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
+                                                           // Variable for the received timestamp
+      while (radio.available()) {                          // While there is data ready
+        radio.read( &myData, sizeof(myData) );             // Get the payload
       }
      
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
+      radio.stopListening();                               // First, stop listening so we can talk  
+      myData.value += 0.01;                                // Increment the float value
+      radio.write( &myData, sizeof(myData) );              // Send the final one back.      
+      radio.startListening();                              // Now, resume listening so we catch the next packets.     
       Serial.print(F("Sent response "));
-      Serial.println(got_time);  
+      Serial.print(myData._micros);  
+      Serial.print(" : ");
+      Serial.println(myData.value);
+      Serial.print("Health: ");
+      Serial.println(myData.health);
    }
  }
 
@@ -130,7 +148,7 @@ if (role == 1)  {
   {
     char c = toupper(Serial.read());
     if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
+      Serial.print(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
       role = 1;                  // Become the primary transmitter (ping out)
     
    }else
@@ -144,4 +162,3 @@ if (role == 1)  {
 
 
 } // Loop
-
