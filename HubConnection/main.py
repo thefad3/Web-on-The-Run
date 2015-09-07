c356f176ef12f5057844449e740c060fd90a462e
Chris Lynch <thefad3@fullsail.edu>
2015-09-07 16:15:52 -0400

    Firebase Save complete.

--- HubConnection/main.py
+++ HubConnection/main.py
@@ -6,20 +6,11 @@ import sys
 import atexit
 from firebase import firebase
 
+_firebase_name = 'mariocarts'
+firebase = firebase.FirebaseApplication('https://' + _firebase_name + '.firebaseio.com/', None)
+
+test = firebase.put('/score', 'Car 1', {'testJson': 'Show me the Data'})
+
 if __name__ == '__main__':
-    freeze_support()
-
-print 'Hello World! Im a Python.'
-
-firebase_name = 'mariocarts'
-
-
-firebase = firebase.FirebaseApplication('https://'+firebase_name+'.firebaseio.com/', None)
-
-test = firebase.put('/score', 'Car 1', {
-    'testJson':'Show me the Data'
-})
-
-print test
-
-exit()
+    def __init__():
+        print('Firebase Test Complete.')
