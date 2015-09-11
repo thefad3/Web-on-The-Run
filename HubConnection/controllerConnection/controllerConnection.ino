c3f10619416329801edf16801f2ffc0a524d07aa
Chris Lynch <thefad3@fullsail.edu>
2015-09-11 03:17:37 -0400

    chaged data stream and cleaned up comments

--- HubConnection/controllerConnection/controllerConnection.ino
+++ HubConnection/controllerConnection/controllerConnection.ino
@@ -7,7 +7,9 @@
 #include <time.h>
 #include <string>
 #include <sstream>
+
 using namespace std;
+
 RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);  
 
 RF24Network network(radio);
@@ -23,21 +25,18 @@ const unsigned long interval = 2000; //ms  // How often to send 'hello world to 
 unsigned long last_sent;             // When did we last send?
 unsigned long packets_sent;          // How many have we sent already
 
-
 struct payload_t {                  // Structure of our payload
   unsigned long ms;
   unsigned long counter;
 };
 
-unsigned int hello;
+unsigned int health;
+unsigned int laps;
+unsigned int powerups;
 
 int main(int argc, char** argv)
 {
-
-    // Refer to RF24.h or nRF24L01 DS for settings
-
   radio.begin();
-  
   delay(5);
   network.begin(/*channel*/ 90, /*node address*/ this_node);
   radio.printDetails();
@@ -53,7 +52,9 @@ int main(int argc, char** argv)
          network.read(header,&payload,sizeof(payload));
       
       printf("Received payload # %lu at %lu \n",payload.counter,payload.ms);
-      hello = payload.counter;
+      health = payload.counter;
+      laps = payload.ms;
+      powerups = "Lap 1 lolz";
       
   CURL *curl;
   CURLcode res;
@@ -64,23 +65,19 @@ int main(int argc, char** argv)
     /* get a curl handle */
   curl = curl_easy_init();
   if(curl) {
-    /* First set the URL that is about to receive our POST. This URL can
-       just as well be a https:// URL if that is what should receive the
-       data. */
+
      curl_easy_setopt(curl, CURLOPT_URL, "https://data.sparkfun.com/input/5JLl6mw4mpU17dn35551?private_key=7BKWRGJoGjiP9qp5NNNP");
-    /* Now specify the POST data */
-      //printf("sdfdsf", payload.counter);
-        //std::string result;
-        //std::stringstream sstm;
-        //sstm << "response=" << hello << "&seconds=kfjhsdk";
-        //result = sstm.str();
-     //String helloo="kjdhfkfhs";
+
+        //Data Stream includes "health" "laps" and "powerups"
+        //Jon's teh fucking man for this
+        
         stringstream ss;
-        ss << "response=" << hello << "&seconds=yomomma";
+        ss << "health=" << health << "&laps=" << laps << "&powerups=" << powerups;
         string s = ss.str();
         const char* cstr = s.c_str();
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, cstr);
-/* Perform the request, res will get the return code */
+        
+    /* Perform the request, res will get the return code */
     res = curl_easy_perform(curl);
     /* Check for errors */
         /* Check for errors */
@@ -92,19 +89,10 @@ int main(int argc, char** argv)
     curl_easy_cleanup(curl);
   }
   curl_global_cleanup();
-
   }     
-     //sleep(2);
      delay(2000);
-     //fclose(pFile);
   }
-
   return 0;
-
-  
-  
 }
-
-    
 
 
